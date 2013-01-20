08ae9b1964fcfc38bb01222ebb19da4f6b7e5c5c
Russell Kaplan <russell.j.kaplan@gmail.com>
2013-01-20 07:58:47 -0500

    asdf

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import jinja2
 import soundcloud
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -17,19 +17,40 @@ def hello():
 def modals():
 	return render_template('modals.html')
 
+@app.route('/sound')
+def sound():
+	return 'hi' #accept .wav and upload to soundcloud
+
 @app.route('/callback')
 def back():
-	return render_template('callback.html')
+	# create client object with app credentials
+	client = soundcloud.Client(client_id='c62653f616ed117b1d19068691b24234',
+	                           client_secret='920ca0969fe79e7d2959ab0cb192e4f4',
+	                           redirect_uri='http://soundsculptor.herokuapp.com/index')
+
+	# if request.method == "POST" or request.method == "GET":
+	# 	return 'hi'
+	# exchange authorization code for access token
+	code = request.args.get('MY_CODE')
+	access_token = client.exchange_token(code)
+	return render_template('callback.html').set_cookie('access_token', access_token)
 
 @app.route('/dropbox')
 def dropbox():
 	return render_template('dropbox.html')
 
-# @app.route('/soundcloud')
-# def soundcloud():
-# 	client = soundcloud.Client(access_token='c62653f616ed117b1d19068691b24234')
+@app.route('/sc')
+def sc():
+	# return render_template('index.html')
+	# create client object with app credentials
+	client = soundcloud.Client(client_id='c62653f616ed117b1d19068691b24234',
+	                           client_secret='920ca0969fe79e7d2959ab0cb192e4f4',
+	                           redirect_uri='http://soundsculptor.herokuapp.com/index')
+
+	return redirect(client.authorize_url())
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
